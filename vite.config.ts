d7ee4b4ea8749e3a0208a97bbd7d1e6e33bc6252
utkarsh arora <arorauttu17@gmail.com>
2024-11-10 12:07:37 +0530

    third commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  build: {
+    outDir: 'dist', // Ensure the output directory is 'dist'
+    target: 'esnext', // Set the target to 'esnext' for modern browsers
+    minify: true, // Minify the output
+    emptyOutDir: true, // Clear the output directory before building
   },
 });
